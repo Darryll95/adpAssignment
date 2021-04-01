d68b88556d5f9131f547b608d181ce53f9ac6ccd
anath <anath@LAPTOP-EO5E1LMJ>
2021-04-01 19:57:55 +0200

    Added a main class for my Java class

---
+++ src/main/java/za/ac/cput/adpgroupassignment/IdentityNumberMain.java
@@ -0,0 +1,17 @@
+
+package za.ac.cput.adpgroupassignment;
+
+/**
+ *
+ * @author anathi mhlom 220006695
+ * 
+ * Main of Identity Number 
+ */
+public class IdentityNumberMain {
+
+    public static void main(String[] args) {
+        
+        System.out.println();
+    }
+    
+}
