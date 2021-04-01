430f9e6e2a924a12d910febe6675fd500f839ff3
nonzwakazim <80507214+nonzwakazim@users.noreply.github.com>
2021-04-02 00:15:19 +0200

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adpgroupassignment;
+
+/**
+ *
+ * @author Nonzwakazi Mgxaji 213181584
+ * 
+ * This class subtracts an integer from another and returns the result
+ */
+public class Subtraction {
+    public int subtraction(int x, int y){
+        int answer = x - y;
+        return answer;
+    }
+}
