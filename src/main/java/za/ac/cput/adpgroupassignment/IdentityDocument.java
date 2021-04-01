89984a059a2f9f9efb9615c8795a11d11383adce
anath <anath@LAPTOP-EO5E1LMJ>
2021-04-01 18:45:59 +0200

    Renamed Java class

---
+++ src/main/java/za/ac/cput/adpgroupassignment/IdentityDocument.java
@@ -0,0 +1,72 @@
+package za.ac.cput.adpgroupassignment;
+
+/**
+ *
+ * @author anathi mhlom 220006695
+ * 
+ * A POJO for Identity Document 
+ */
+public class IdentityDocument {
+    private String Surname, Names, Sex, CountryOfBirth, Status;
+    private int IdentityNumber, DateOfBirth;
+    
+    public void getSurname{
+    return Surname;
+}
+    
+    public void setSurname(String Surname){
+    this.Surname = Surname;
+}
+    
+    public void getNames{
+    return Names;
+}
+    
+    public void setNames(String Names){
+    this.Names = Names;
+}
+    
+    public void getSex{
+    return Sex;
+}
+    
+    public void setSex(String Sex){
+    this.Sex = Sex;
+}
+    
+    public void getCountryOfBirth{
+    return CountryOfBirth;
+}
+    
+    public void setCountryOfBirth(String CountryOfBirth){
+    this.CountryOfBirth = CountryOfBirth;
+}
+    
+    public void getStatus{
+    return Status;
+}
+    
+    public void setStatus(String Status){
+    this.Status = Status;
+}
+    
+    public void getIdentityNumber{
+    return IdentityNumber;
+}
+    
+    public void setIdentityNumber(int IdentityNumber){
+    this.IdentityNumber = IdentityNumber;
+}
+    
+    public void getDateOfBirth{
+    return DateOfBirth;
+}
+    
+    public void setDateOfBirth(int DateOfBirth){
+    this.DateOfBirth = DateOfBirth;
+}
+}
+
+
+    
+
